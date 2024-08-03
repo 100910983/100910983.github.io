8643345b8d96f7d44dd2505afb23c75429c86b23
100910983 <basanta.khatiwada@dcmail.ca>
2024-08-03 15:16:58 -0400

    added function to generate random rgb color in function ball

--- Assignment 4/part 4/main-finished.js
+++ Assignment 4/part 4/main-finished.js
@@ -12,21 +12,23 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+function Shape(x, y, velX, velY, exist) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.exist = exist;
 }
-
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
+function Ball(x, y, velX, velY, color, size, exist) {
+  Shape.call(this, x, y, velX, velY, exist);
+  this.color = color;
+  this.size = size;
+}
+function Ball(x, y, velX, velY, color, size, exist) {
+  Shape.call(this, x, y, velX, velY, exist);
+  this.color = color;
+  this.size = size;
+}
 
   draw() {
     ctx.beginPath();
